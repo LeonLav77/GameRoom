45226633e1a38d2e06bfbd81382d22fadd2840eb
Lejon <52333278+LeonLav77@users.noreply.github.com>
2021-09-01 12:31:03 +0200

    not much

--- game/resources/views/gameRoom.blade.php
+++ game/resources/views/gameRoom.blade.php
@@ -13,7 +13,29 @@
         channel.listen('GameRoomJoin', function(data) {
             console.log(data);
         });
-
+        function putX(id){
+            console.log(window.array);
+            $currentElement = $('#'+id);
+            let child = document.createElement('h1');
+            child.setAttribute('name','X');
+            child.innerHTML = 'X';
+            $currentElement.append(child);
+            // $currentElement.text('X');
+        }
+        $(document).ready(function(){
+            window.array = [];
+            $('span').each(function () {
+                window.array.push(this);
+            });
+            for (var i = 0; i < window.array.length; i++) {
+                $(window.array[i]).click(function () {
+                    putX((this.id));
+                    console.log((this.id).split("-")[1]);
+                }
+                );
+            }
+            putX();
+        });
     </script>
 </head>
 <body class="">
